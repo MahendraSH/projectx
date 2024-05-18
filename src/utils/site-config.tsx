e3da0eb2e43bd91c2e5288e6e6c3bd8c647dcb1d
MahendraSH <maahi2001sh@gmail.com>
2024-05-18 18:41:04 +0530

    testing seo

--- src/utils/site-config.tsx
+++ src/utils/site-config.tsx
@@ -8,5 +8,5 @@ rest assured that you'll receive a completely unique
 design each time. Once a t-shirt design is gone, it's forever gone,
 ensuring that you own a piece of fashion
     history that's truly one-of-a-kind.`,
-  url: "https://projectx-dev.vercel.app/",
+  url: "https://projectx-dev.vercel.app",
 };
